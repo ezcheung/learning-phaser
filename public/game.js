b10e0d05f4acce5c80d1dee3e78e7fa33337fbdd
Elliot Cheung <elliotccheung@gmail.com>
2016-11-18 20:21:35 +0000

    Rendered player and can move with cursors

--- public/game.js
+++ public/game.js
@@ -1,5 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+let cursors;
+let player;
+
 function preload(){
   game.load.image("background", "assets/background.png");
   game.load.image("blacksquare", "assets/blacksquare.png");
@@ -7,9 +10,27 @@ function preload(){
 }
 
 function create(){
+  game.physics.startSystem(Phaser.Physics.ARCADE);
   game.add.sprite(0, 0, "background");
+  player = game.add.sprite(375, 550, "player");
+
+  game.physics.arcade.enable(player);
+  player.body.collideWorldBounds = true;
+
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update(){
+  player.body.velocity.x = 0;
 
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+    }
 }
