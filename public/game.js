81d14fe22bfa6550f670267f03b5d3652f16bef2
Elliot Cheung <elliotccheung@gmail.com>
2016-11-18 20:11:05 +0000

    Got phaser skeleton running

--- public/game.js
+++ public/game.js
@@ -1,1 +1,15 @@
-game.js
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload(){
+  game.load.image("background", "assets/background.png");
+  game.load.image("blacksquare", "assets/blacksquare.png");
+  game.load.image("player", "assets/christmas_tiles.png");
+}
+
+function create(){
+  game.add.sprite(0, 0, "background");
+}
+
+function update(){
+
+}
